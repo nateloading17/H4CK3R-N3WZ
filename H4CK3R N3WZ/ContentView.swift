6e52b25c7c0d8e4b206ec1c88e82f082b883bc4a
nateloading17 <nathanmilobaez@gmail.com>
2024-07-09 15:10:40 -0400

    Initial Commit

---
+++ H4CK3R N3WZ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  H4CK3R N3WZ
+//
+//  Created by Nathaniel Baez on 7/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ H4CK3R N3WZ/H4CK3R_N3WZApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4CK3R_N3WZApp.swift
+//  H4CK3R N3WZ
+//
+//  Created by Nathaniel Baez on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct H4CK3R_N3WZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
