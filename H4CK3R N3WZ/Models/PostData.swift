cf547f36fed8e53365e2c2a61ec1f76cb7ec6421
nateloading17 <nathanmilobaez@gmail.com>
2024-07-29 19:31:40 -0400

    Complete

--- H4CK3R N3WZ/Models/PostData.swift
+++ H4CK3R N3WZ/Models/PostData.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+    
+    var id: String {
+        return objectID
+    }
+}
